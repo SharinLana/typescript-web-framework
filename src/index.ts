e22324b13ed1c9b629070a2f4ba20c2d9d663394
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-10 16:30:06 -0700

    created a set method in the User class that accepts an object with the user properties (or a single property) and changes the initial User state

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import {User} from "./models/User";
+import { User } from "./models/User";
 
-const user = new User({name: "John", age: 25});
+const user = new User({ name: "John", age: 25 });
+
+user.set({ name: "Martha" });
 console.log(user.get("name")); // John
