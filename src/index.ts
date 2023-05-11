bc165878a4c3f49794b26756f9477c884f4a67c8
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-11 13:54:14 -0700

    made the events User property public + tested the result of the event listeners in index.ts  after refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "Max", age: 76 });
-user.save();
-// check the result ib the browser => devtools => Fetch/XHR
+user.events.on("click", () => {
+  console.log("Clicked!")
+});
+user.events.trigger("click")
+
