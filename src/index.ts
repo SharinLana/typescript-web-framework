0bebd5cd9b74f67221afb3da83befe6d9f7ab553
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-10 17:03:13 -0700

    checked the on() method in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({ name: "John", age: 25 });
 
-user.set({ name: "Martha" });
-console.log(user.get("name")); // John
+user.on("click", () => {})
+user.on("click", () => {})
+user.on("mouseover", () => {})
+
+console.log(user)
