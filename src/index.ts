cff8db5c6a11566663ee01324bc46e4137a5e8f6
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-11 13:11:12 -0700

    checked the save method for the new user

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
-
-user.set({ name: "Helena", age: 39 });
+const user = new User({ name: "Max", age: 76 });
 user.save();
 // check the result ib the browser => devtools => Fetch/XHR
-
