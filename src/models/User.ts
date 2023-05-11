061166a63488f62e40425e875b8c957c0f3990f4
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-11 13:02:00 -0700

    creates a save method in the User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -45,4 +45,14 @@ export class User {
         this.set(response.data)
       });
   }
+
+  save(): void {
+    if (this.get("id")) {
+      // put request
+      axios.put(`http://localhost:3000/users/${this.get("id")}`, this.data)
+    } else {
+      // post request
+      axios.post("http://localhost:3000/users", this.data)
+    }
+  }
 }
