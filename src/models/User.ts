068da0f31be4aced2774e4adaac91f1969a217aa
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-12 18:21:46 -0700

    started refactoring of the User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,4 @@
-import { AxiosResponse } from "axios";
-import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
-import { Attributes } from "./Attributes";
+import { Model } from "./Model";
 
 export interface UserProps {
   id?: number;
@@ -11,14 +8,4 @@ export interface UserProps {
 
 const rootUrl = "http://localhost:3000/users"; // available in the console after running: npm rum start:db
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  
-}
+export class User extends Model<UserProps> {}
